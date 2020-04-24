6ac4e9918ba9ff85dab596b6d0c2be427badb8b7
Diego <juanmedina0505@gmail.com>
2020-04-24 11:44:39 -0500

    Initial Commit - Project Structure :tada::tada::tada:

---
+++ src/routes/routes.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Routes, Switch, Route } from "react-router-dom";
+import Home from "../views/home";
+// import SignIn from "../views/signIn";
+// import SignUp from "../views/signUp";
+// import Groups from "../views/groups";
+// import Events from "../views/events";
+// import GroupProfile from "../views/groupProfile";
+// import EventProfile from "../views/eventProfile";
+// import UserProfile from "../views/userProfile";
+// import EditGroup from "../views/editGroup";
+// import EditEvent from "../views/editEvent";
+// import EditProfile from "../views/editProfile";
+// import CreateGroup from "../views/createGroup";
+// import CreateEvent from "../views/createEvent";
+// import CreateProfile from "../views/createProfile";
+// import NotFound from "../views/notFound";
+
+const routes = () => {
+  return (
+    <Routes>
+      <Switch>
+        {/* afuera del MainLayout para que no tengan navbar o footer */}
+        {/* <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/signup" component={SignUp} /> */}
+        {/* <MainLayout> */}
+        {/* <Redirect exact from="/" to="home" /> */}
+        <Route exact path="/home" component={Home} />
+        {/* </MainLayout> */}
+      </Switch>
+    </Routes>
+  );
+};
+
+export default routes;
