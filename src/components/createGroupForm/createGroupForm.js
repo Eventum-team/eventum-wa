4d25b48f3fbe3586d1c67c41373e1ca6464a0507
gabriel <gabriel19oct@hotmail.com>
2020-05-02 21:50:48 -0500

    :sprakles: added create group view

--- src/components/createGroupForm/createGroupForm.js
+++ src/components/createGroupForm/createGroupForm.js
@@ -1,127 +1,85 @@
 import React from "react";
 import "antd/dist/antd.css";
 import { Form, Select, Input, Button, DatePicker } from "antd";
+import CreateGroupForm from "../dropDownGroup/dropDownGroup";
 
 const { Option } = Select;
 
+const prefixSelector = (
+  <Form.Item name="prefix" noStyle>
+    <Select style={{ width: 70 }}>
+      <Option value="01">+01</Option>
+      <Option value="54">+54</Option>
+    </Select>
+  </Form.Item>
+);
+
 const CreateGroup = (props) => {
-  const { handleSubmit } = props;
-
-  const config = {
-    rules: [{ type: "object", required: true, message: "Please select time!" }],
-  };
-
-  // const handleSelectChange = (value) => {
-  //   props.form.setFieldsValue({
-  //     note: `Hi, ${value === "male" ? "man" : "lady"}!`,
-  //   });
-  // };
-
-  const checkPrice = (rule, value, callback) => {
-    if (value > 0) {
-      return callback();
-    }
-    callback("Price must greater than zero!");
-  };
+  const { handleSubmit, groupTypes } = props;
 
   return (
-    <Form
-      labelCol={{ span: 5 }}
-      // wrapperCol={{ span: 112 }}
-      onSubmit={handleSubmit}
-      layout="vertical"
-    >
-      <Form.Item
-        label="Fecha Viaje"
-        // type="flex"
-        // labelAlign="left"
-        // justify="center"
-        // labelCol={{ span: 2, offset: 0 }}
-        wrapperCol={{ span: 24, offset: 0 }}
+      <Form
+        labelCol={{ span: 5 }}
+        // wrapperCol={{ span: 112 }}
+        onFinish={handleSubmit}
+        layout="vertical"
+        id="createGroup"
       >
-        {/* {getFieldDecorator("date", config) */}
-        {/* (<DatePicker placeholder="" />)} */}
-        <DatePicker placeholder="" />
-      </Form.Item>
-      <Form.Item
-        label="Tipo Viaje"
-        // type="flex"
-        // labelAlign="left"
-        // justify="center"
-        // labelCol={{ span: 2, offset: 0 }}
-        wrapperCol={{ span: 24, offset: 0 }}
-        rules={[{ required: true, message: "Por favor seleccione un tipo" }]}
-      >
-        <Select
-          placeholder="Seleccione el tipo de viaje"
-          // onChange={handleSelectChange}
+        <Form.Item
+          label="Tipo Viaje"
+          wrapperCol={{ span: 24, offset: 0 }}
+          name="groupType"
+          rules={[{ required: true, message: "Por favor seleccione un tipo" }]}
         >
-          <Select.Option value="sell">Venta</Select.Option>
-          <Select.Option value="buy">compra</Select.Option>
-        </Select>
-      </Form.Item>
-      <Form.Item
-        label="Nombre"
-        // type="flex"
-        labelAlign="right"
-        // justify="center"
-        labelCol={{ span: 6 }}
-        wrapperCol={{ span: 24, offset: 0 }}
-        rules={[{ required: true, message: "Please input your note!" }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Lugar"
-        // type="flex"
-        // labelAlign="left"
-        // justify="center"
-        // labelCol={{ span: 2, offset: 0 }}
-        wrapperCol={{ span: 24, offset: 0 }}
-        rules={[{ required: true, message: "Please input your note!" }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Valor "
-        // type="flex"
-        // labelAlign="left"
-        // justify="center"
-        // labelCol={{ span: 2, offset: 0 }}
-        wrapperCol={{ span: 24, offset: 0 }}
-        rules={[
-          {
-            required: true,
-            message: "Please input your note!",
-            validator: checkPrice,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Cantidad"
-        // type="flex"
-        // labelAlign="left"
-        // justify="center"
-        // labelCol={{ span: 2, offset: 0 }}
-        wrapperCol={{ span: 24, offset: 0 }}
-        rules={[{ required: true, message: "Please input your note!" }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        type="flex"
-        labelAlign="center"
-        justify="center"
-        labelCol={{ span: 4, offset: 0 }}
-        // wrapperCol={{ span: 11, offset: 0 }}
-      >
-        <Button type="primary" htmlType="submit">
-          Crear
-        </Button>
-      </Form.Item>
-    </Form>
+          <Select
+            placeholder="Seleccione el tipo de viaje"
+            // onChange={handleSelectChange}
+          >
+            {groupTypes.groupTypes.map((value) => (
+              <Option key={value.id_type} value={value.id_type}>{value.name}</Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Nombre"
+          labelAlign="right"
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 24, offset: 0 }}
+          rules={[{ required: true, message: "Ingresa un nombre" }]}
+          name="groupName"
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Descripcion"
+          wrapperCol={{ span: 24, offset: 0 }}
+          name="description"
+          rules={[{ required: true, message: "Ingresa una descripcion" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="phone"
+          label="Numero de celular"
+          labelAlign="left"
+          labelCol={{ span: 6 }}
+          labelAlign="right"
+          wrapperCol={{ span: 24, offset: 0 }}
+        >
+          <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
+        </Form.Item>
+        <Form.Item
+          type="flex"
+          labelAlign="center"
+          justify="center"
+          labelCol={{ span: 4, offset: 0 }}
+          // wrapperCol={{ span: 11, offset: 0 }}
+        >
+          <Button type="primary" htmlType="submit">
+            Crear
+          </Button>
+        </Form.Item>
+      </Form>
   );
 };
 
--- src/views/createGroup/createGroup.js
+++ src/views/createGroup/createGroup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CreateGroupForm from "../../components/createGroupForm";
 import MainLayout from "../../components/layout";
 import { Typography, Row, Col } from "antd";
@@ -8,41 +8,118 @@ import Spinner from "../../components/spinner";
 import ErrorAlert from "../../components/error";
 import Successful from "../../components/succesfulOperation";
 import backgroundImage from "../../assets/imgs/undraw_schedule_pnbk.png";
+import {  useMutation, useQuery } from "@apollo/react-hooks";
+import gql from 'graphql-tag';
 
 const { Title } = Typography;
 
+
+const GROUP_TYPES_QUERY = gql`
+query {
+  groupTypes{
+    id_type
+    name
+  }
+}
+`;
+
+/*** 
+const EVENT_MUTATION = gql`
+mutation {
+  createEvent(
+    input:{
+      ownerId: 1
+      ownerType: "user"
+      name: "test event"
+      eventStartDate: "2020-04-20T06:00:00Z"
+      eventFinishDate: "2020-04-20T06:00:00Z"
+      description: "desc"
+      latitude: "1"
+      longitude: "2"
+      status: "Ok"
+      eventType: "official"
+      url: "some url"
+    }
+  ){
+    message
+    status
+  }
+}
+`;*/
+
+
+const CREATE_GROUP_MUTATION = gql`
+mutation CreateNewGroup(
+  $id_type: Int!,
+  $name: String!,
+  $description: String!,
+  $contact_number: String!,
+  $status: String!,
+  $token: String!,
+  $id_user: ID!,
+){
+  createNewGroup(
+    id_user: $id_user
+    input:{
+      id_type: $id_type
+      name: $name
+      description: $description
+      contact_number: $contact_number
+      status: $status
+    }
+    token: {
+      token: $token
+    }
+  ){
+    name
+  }
+}
+`;
+
 const CreateGroup = (props) => {
   // const dispatch = useDispatch();
-  const error = null;
-  const pending = false;
-  const successful = false;
+  const [successful, setSuccessful] = useState(false);
+  const { loading, error, data } = useQuery(GROUP_TYPES_QUERY);
+  const [createGroupMutation, { loadingCreate}] = useMutation(CREATE_GROUP_MUTATION, { errorPolicy: 'all' });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.form.validateFields((err, values) => {
-      if (!err) {
+  const handleSubmit = (values) => {
+    console.log(values);
+    createGroupMutation({
+      variables:{
+        id_type: parseInt(values.groupType),
+        name: values.groupName,
+        description: values.description,
+        contact_number: values.phone,
+        status: "Ok",
+        token: "token",
+        id_user: parseInt("1")
       }
     });
+    setSuccessful(true);
   };
 
+  console.log(data);
+
+  // {!loading && error && <ErrorAlert error={error} />}
   return (
     <ContentLayout>
-      {pending && <Spinner />}
-      {!pending && error && <ErrorAlert error={error} />}
-      {!pending && !error && successful && (
+      {loading && <Spinner />}
+      
+      {!loading && successful && (
         <Successful
           redirect={"createGroup"}
+          processCompleted={()=>{}}
           //terminar proceso
         />
       )}
-      {!pending && !error && !successful && (
+      {!loading  && !successful && (
         <div>
           <Title level={2} style={{ textAlign: "center" }}>
             Crea un Grupo
           </Title>
           <Row>
             <Col span={12} style={{ padding: "0 100px" }}>
-              <CreateGroupForm handleSubmit={handleSubmit} />
+              <CreateGroupForm handleSubmit={handleSubmit} groupTypes={data} />
             </Col>
             <Col span={12}>
               <div>
