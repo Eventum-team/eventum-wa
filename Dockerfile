816f91eecdf9b289d5afe83243806e7d310a0d32
juandiego <juanmedina0505@gmail.com>
2020-06-21 00:05:10 -0500

    .env values are usable now

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM node:12
 
 WORKDIR /app
 
+ENV REACT_APP_API_IP=$REACT_APP_API_IP
+ENV REACT_APP_API_PORT=$REACT_APP_API_PORT
+
 COPY package*.json ./
 
 RUN npm install
--- src/server.js
+++ src/server.js
@@ -1,10 +1,6 @@
-
-const ipApi = "ev-api-service-clusterip";
-const portApi = "5000";
-
+const ipApi = process.env.REACT_APP_API_IP;
+const portApi = process.env.REACT_APP_API_PORT;
 
 export const ipApiGetway = `http://${ipApi}:${portApi}/graphql`;
 //export const ipLoadImages = 'http://190.24.19.228:5000/upload1';
 export const ipLoadImages = `http://${ipApi}:${portApi}/upload1`;
-
-
